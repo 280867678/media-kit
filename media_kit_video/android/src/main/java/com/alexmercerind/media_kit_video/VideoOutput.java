8512acbb7eb9d0b7fb12270c63f7b047b9b4263a
280867678 <118792806+280867678@users.noreply.github.com>
2026-05-21 11:03:41 +0800

    Update VideoOutput.java

--- media_kit_video/android/src/main/java/com/alexmercerind/media_kit_video/VideoOutput.java
+++ media_kit_video/android/src/main/java/com/alexmercerind/media_kit_video/VideoOutput.java
@@ -7,156 +7,128 @@
  */
 package com.alexmercerind.media_kit_video;
 
-import android.app.Activity;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.Surface;
 
-import androidx.annotation.NonNull;
-
-import java.util.HashMap;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Locale;
 import java.util.Objects;
 
-import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.embedding.engine.plugins.activity.ActivityAware;
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.view.TextureRegistry;
 
-/**
- * MediaKitVideoPlugin
- */
-public class MediaKitVideoPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
-    public static Activity activity;
-    private MethodChannel channel;
-    private TextureRegistry textureRegistry;
-    private VideoOutputManager videoOutputManager;
+public class VideoOutput {
+    private static final String TAG = "VideoOutput";
+    private static final Method newGlobalObjectRef;
+    private static final Method deleteGlobalObjectRef;
+    private static final HashSet<Long> deletedGlobalObjectRefs = new HashSet<>();
+    private static final Handler handler = new Handler(Looper.getMainLooper());
+
+    static {
+        try {
+            Class<?> mediaKitAndroidHelperClass = Class.forName("com.alexmercerind.mediakitandroidhelper.MediaKitAndroidHelper");
+            newGlobalObjectRef = mediaKitAndroidHelperClass.getDeclaredMethod("newGlobalObjectRef", Object.class);
+            deleteGlobalObjectRef = mediaKitAndroidHelperClass.getDeclaredMethod("deleteGlobalObjectRef", long.class);
+            newGlobalObjectRef.setAccessible(true);
+            deleteGlobalObjectRef.setAccessible(true);
+        } catch (Throwable e) {
+            Log.i("media_kit", "package:media_kit_libs_android_video missing.");
+            throw new RuntimeException("Failed to initialize VideoOutput.");
+        }
+    }
+
+    private long id = 0;
+    private long wid = 0;
+    private int width = 0;
+    private int height = 0;
+
+    private final TextureUpdateCallback textureUpdateCallback;
+    private final TextureRegistry.SurfaceTextureEntry surfaceTextureEntry;
+    private final Surface surface;
+
     private final Object lock = new Object();
 
-    @Override
-    public void onAttachedToActivity(@NonNull ActivityPluginBinding activityPluginBinding) {
-        synchronized (lock) {
-            MediaKitVideoPlugin.activity = activityPluginBinding.getActivity();
+    VideoOutput(TextureRegistry textureRegistryReference, TextureUpdateCallback textureUpdateCallback) {
+        this.textureUpdateCallback = textureUpdateCallback;
+        this.surfaceTextureEntry = textureRegistryReference.createSurfaceTexture();
+        this.surface = new Surface(surfaceTextureEntry.surfaceTexture());
+        this.id = surfaceTextureEntry.id();
+        onSurfaceAvailable();
+    }
 
-            if (videoOutputManager == null) {
-                if (MediaKitVideoPlugin.activity != null && channel != null && textureRegistry != null) {
-                    videoOutputManager = new VideoOutputManager(channel, textureRegistry);
-                    lock.notifyAll();
+    public void dispose() {
+        synchronized (lock) {
+            try {
+                surface.release();
+            } catch (Throwable e) {
+                Log.e(TAG, "dispose", e);
+            }
+            try {
+                surfaceTextureEntry.release();
+            } catch (Throwable e) {
+                Log.e(TAG, "dispose", e);
+            }
+            onSurfaceCleanup();
+        }
+    }
+
+    public void setSurfaceSize(int width, int height) {
+        setSurfaceSize(width, height, false);
+    }
+
+    private void setSurfaceSize(int width, int height, boolean force) {
+        synchronized (lock) {
+            try {
+                if (!force && this.width == width && this.height == height) {
+                    return;
                 }
+                this.width = width;
+                this.height = height;
+                surfaceTextureEntry.surfaceTexture().setDefaultBufferSize(width, height);
+                onSurfaceAvailable();
+            } catch (Throwable e) {
+                Log.e(TAG, "setSurfaceSize", e);
             }
         }
     }
 
-    @Override
-    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding activityPluginBinding) {
+    public void onSurfaceAvailable() {
         synchronized (lock) {
-            MediaKitVideoPlugin.activity = activityPluginBinding.getActivity();
+            id = surfaceTextureEntry.id();
+            wid = newGlobalObjectRef(surface);
+            textureUpdateCallback.onTextureUpdate(id, wid, width, height);
+        }
+    }
 
-            if (videoOutputManager == null) {
-                if (MediaKitVideoPlugin.activity != null && channel != null && textureRegistry != null) {
-                    videoOutputManager = new VideoOutputManager(channel, textureRegistry);
-                    lock.notifyAll();
-                }
+    public void onSurfaceCleanup() {
+        synchronized (lock) {
+            textureUpdateCallback.onTextureUpdate(id, 0, width, height);
+            if (wid != 0) {
+                final long widReference = wid;
+                handler.postDelayed(() -> deleteGlobalObjectRef(widReference), 5000);
             }
         }
     }
 
-    @Override
-    public void onDetachedFromActivityForConfigChanges() {
-    }
-
-    @Override
-    public void onDetachedFromActivity() {
-    }
-
-    @Override
-    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        synchronized (lock) {
-            channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "com.alexmercerind/media_kit_video");
-            textureRegistry = flutterPluginBinding.getTextureRegistry();
-
-            channel.setMethodCallHandler(this);
-
-            if (videoOutputManager == null) {
-                if (MediaKitVideoPlugin.activity != null && channel != null && textureRegistry != null) {
-                    videoOutputManager = new VideoOutputManager(channel, textureRegistry);
-                    lock.notifyAll();
-                }
-            }
+    private static long newGlobalObjectRef(Object object) {
+        try {
+            return (long) Objects.requireNonNull(newGlobalObjectRef.invoke(null, object));
+        } catch (Throwable e) {
+            Log.e(TAG, "newGlobalObjectRef", e);
+            return 0;
         }
     }
 
-    @Override
-    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-        synchronized (lock) {
-            while (videoOutputManager == null) {
-                try {
-                    lock.wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            switch (call.method) {
-                case "VideoOutputManager.Create": {
-                    final String handle = call.argument("handle");
-                    if (handle != null) {
-                        final VideoOutput videoOutput = videoOutputManager.create(Long.parseLong(handle));
-                        final HashMap<String, Long> data = new HashMap<>();
-                        data.put("id", videoOutput.id);
-                        result.success(data);
-                    } else {
-                        result.success(null);
-                    }
-                    break;
-                }
-                case "VideoOutputManager.CreateSurface": {
-                    final String handle = call.argument("handle");
-                    if (handle != null) {
-                        final long wid = videoOutputManager.createSurface(Long.parseLong(handle));
-                        final HashMap<String, Long> data = new HashMap<>();
-                        data.put("wid", wid);
-                        result.success(data);
-                    } else {
-                        result.success(null);
-                    }
-                    break;
-                }
-                case "VideoOutputManager.SetSurfaceTextureSize": {
-                    final String handle = call.argument("handle");
-                    final String width = call.argument("width");
-                    final String height = call.argument("height");
-                    if (handle != null) {
-                        videoOutputManager.setSurfaceTextureSize(
-                                Long.parseLong(handle),
-                                Integer.parseInt(Objects.requireNonNull(width)),
-                                Integer.parseInt(Objects.requireNonNull(height))
-                        );
-                    }
-                    result.success(null);
-                    break;
-                }
-                case "VideoOutputManager.Dispose": {
-                    final String handle = call.argument("handle");
-                    if (handle != null) {
-                        videoOutputManager.dispose(Long.parseLong(handle));
-                    }
-                    result.success(null);
-                    break;
-                }
-                case "Utils.IsEmulator": {
-                    result.success(Utils.isEmulator());
-                    break;
-                }
-                default: {
-                    result.notImplemented();
-                    break;
-                }
-            }
+    private static void deleteGlobalObjectRef(long ref) {
+        if (deletedGlobalObjectRefs.contains(ref)) return;
+        if (deletedGlobalObjectRefs.size() > 100) deletedGlobalObjectRefs.clear();
+        deletedGlobalObjectRefs.add(ref);
+        try {
+            deleteGlobalObjectRef.invoke(null, ref);
+        } catch (Throwable e) {
+            Log.e(TAG, "deleteGlobalObjectRef", e);
         }
-    }
-
-    @Override
-    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-        channel.setMethodCallHandler(null);
     }
 }
